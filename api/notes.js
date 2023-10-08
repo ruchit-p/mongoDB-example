e1a25c17ce49f18c1f145cf99ea7e4e1b9395708
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2023-10-07 20:09:14 -0400

    updated notes.js

--- api/notes.js
+++ api/notes.js
@@ -1,8 +1,33 @@
-// addNote.js
-const mongoose = require('mongoose');
-const { noteSchema } = require('../lib/models');
+const mongoose = require("mongoose");
 
+// Define your note schema and model only if it hasn't been defined yet.
+// This prevents "OverwriteModelError" which occurs when the schema is recompiled.
+if (!mongoose.models.Note) {
+  const noteSchema = new mongoose.Schema({
+    title: String,
+    content: String
+  });
+  mongoose.model("Note", noteSchema);
+}
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model("Note");
 
-// rest of your code
+module.exports = async (req, res) => {
+  try {
+    // Connect to MongoDB using the environment variable from Vercel
+    const pass = process.env.PASSWORD;
+    await mongoose.connect(`mongodb+srv://admin:${pass}@cluster0.mgwl11z.mongodb.net/`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // Fetch notes
+    const notes = await Note.find({});
+
+    // Close the MongoDB connection
+    await mongoose.connection.close();
+
+    // Send the notes as JSON
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred while fetching notes." });
+  }
+};
