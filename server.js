67c36e5728e59e9abaa4ee8532fe003727c7bbfe
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2023-10-07 20:42:40 -0400

    Reverting to commit 986dcf3

--- server.js
+++ server.js
@@ -2,50 +2,32 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const path = require("path");
-require('dotenv').config();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json()); // for parsing application/json
 
+mongoose.connect("mongodb+srv://ruchitp:Kalpesh1527@test.ns67y.mongodb.net/userInfo", {useNewUrlParser: true}, {useUnifiedTopology: true})
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-
-// Create a data schema
+//create a data schema
 const noteSchema = {
     title: String,
     content: String
-};
+}
 
 const Note = mongoose.model("Note", noteSchema);
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+    res.sendFile(__dirname + "/index.html");
+})
 
-app.get('/notes', function(req, res) {
-    Note.find({}, function(err, notes){
-        if(err) {
-            console.log(err);
-        } else {
-            res.json(notes);
-        }
-    });
-});
-
-app.get('/index', function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.post("/", function(req, res) {
-    let newNote = new Note({
+app.post("/", function(req, res){
+    let newNote = Note({
         title: req.body.title,
         content: req.body.content
-    });
+    })
     newNote.save();
-    res.redirect("/index");
-});
+    res.redirect("/");
+})
 
 app.listen(3000, function() {
-    console.log("server is running on port 3000, http://localhost:3000");
-});
+    console.log("server is running");
+})
